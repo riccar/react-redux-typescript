3a0ecbabc36b11469f6ef2721e7ed25b7229706c
Ricardo Carballo <riccar@protonmail.com>
2019-07-02 13:56:13 +1000

    Refactor index action to only exports from todos actions file and from the types. New todos.ts action file. Using type union to annotate all the reducers action types without cluttering the reduer. Completed the delete todo feature and annotate the AppProps action creators with a typeof of the actual action when possible.

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,4 +1,17 @@
+import { FetchTodosAction, DeleteTodoAction } from './todos';
+
 export enum ActionTypes {
   fetchTodos,
   deleteTodo
 }
+
+/**
+ * Create a type alias to add all the action creators interfaces asn a Union type
+ * By doing this, the reducer is de-cluttered from having all the possible actions types
+ * annotated in the same line.
+ *
+ * In addition, in conjunction with the union above, Typescript provides a type guard
+ * that knows on every reducer switch statement what type from the Union it
+ * should refers to.
+ */
+export type Action = FetchTodosAction | DeleteTodoAction;
--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,10 +1,16 @@
-import { Todo, FetchTodosAction, DeleteTodoAction } from '../actions';
-import { ActionTypes } from '../actions/types';
+import { Todo, Action, ActionTypes } from '../actions';
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+/**
+ * By creating the action types enum and the action Union types, Typescript
+ * provides a type guard that knows on every reducer switch statement
+ * what type from the Union it should refers to.
+ */
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter(todo => todo.id !== action.payload);
     default:
       return state;
   }
